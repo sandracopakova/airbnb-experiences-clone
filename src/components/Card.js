b28d4fec3815e290b70fac6ac8d3e4a99dc53086
Hana Čopáková <sandracopakova@gmail.com>
2021-12-30 20:12:34 +0100

    rewrite data render

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,27 +1,27 @@
 import React from "react";
 import star from "../images/star.png";
 
-export default function Card({ img, rating, reviewCount, location, title, price, openSpots }) {
+export default function Card(props) {
   let badgeText;
-  if (openSpots === 0) {
+  if (props.item.openSpots === 0) {
     badgeText = "SOLD OUT";
-  } else if (location === "Online") {
+  } else if (props.item.location === "Online") {
     badgeText = "ONLINE";
   }
   return (
     <div className="card">
       {badgeText && <div className="card__badge">{badgeText}</div>}
-      <img src={`/img/${img}`} className="card__img" />
+      <img src={`/img/${props.item.coverImg}`} className="card__img" />
       <div>
         <img src={star} className="card__star" />
-        <span> {rating}</span>
+        <span> {props.item.stats.rating}</span>
         <span className="card__text--light">
-          ({reviewCount}) • {location}
+          ({props.item.stats.reviewCount}) • {props.item.location}
         </span>
       </div>
-      <div>{title}</div>
+      <div>{props.item.title}</div>
       <div>
-        <span className="card__text--bold">From {price}</span> / person
+        <span className="card__text--bold">From {props.item.price}</span> / person
       </div>
     </div>
   );
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -4,18 +4,8 @@ import data from "../data";
 
 export default function Cards() {
   const cards = data.map((item) => {
-    return (
-      <Card
-        key={item.id}
-        img={item.coverImg}
-        rating={item.stats.rating}
-        reviewCount={item.stats.reviewCount}
-        location={item.location}
-        title={item.title}
-        price={item.price}
-        openSpots={item.openSpots}
-      />
-    );
+    return <Card key={item.id} item={item} />;
   });
+
   return <section className="cards">{cards}</section>;
 }
