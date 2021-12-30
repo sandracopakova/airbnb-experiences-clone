7e5f712fdec7e07d9fcea78373fdf7f0f612bc99
Hana Čopáková <sandracopakova@gmail.com>
2021-12-30 19:25:43 +0100

    add js badge text on card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,20 @@
 import React from "react";
 import star from "../images/star.png";
 
-export default function Card({ img, rating, reviewCount, location, title, price }) {
+export default function Card({ img, rating, reviewCount, location, title, price, openSpots }) {
+  let badgeText;
+  if (openSpots === 0) {
+    badgeText = "SOLD OUT";
+  } else if (location === "Online") {
+    badgeText = "ONLINE";
+  }
   return (
     <div className="card">
+      {badgeText && <div className="card__badge">{badgeText}</div>}
       <img src={`/img/${img}`} className="card__img" />
       <div>
         <img src={star} className="card__star" />
-        <span>{rating}</span>
+        <span> {rating}</span>
         <span className="card__text--light">
           ({reviewCount}) • {location}
         </span>
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -4,7 +4,18 @@ import data from "../data";
 
 export default function Cards() {
   const cards = data.map((item) => {
-    return <Card img={item.coverImg} rating={item.stats.rating} reviewCount={item.stats.reviewCount} location={item.location} title={item.title} price={item.price} />;
+    return (
+      <Card
+        key={item.id}
+        img={item.coverImg}
+        rating={item.stats.rating}
+        reviewCount={item.stats.reviewCount}
+        location={item.location}
+        title={item.title}
+        price={item.price}
+        openSpots={item.openSpots}
+      />
+    );
   });
   return <section className="cards">{cards}</section>;
 }
